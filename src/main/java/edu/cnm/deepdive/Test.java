c1932c35859b26e1b7716ace6fbc576398b60bd8
Nick Bennett <nick@nickbenn.com>
2021-10-11 17:28:29 -0600

    Use Comparator.comparingInt.

--- src/main/java/edu/cnm/deepdive/Test.java
+++ src/main/java/edu/cnm/deepdive/Test.java
@@ -25,8 +25,8 @@ public class Test {
     Collections.sort(deck);
     System.out.println(deck);
     int numRanks = Rank.values().length;
-    deck.sort((card1, card2) -> (card1.getRank().ordinal() + numRanks - 1) % numRanks
-        - (card2.getRank().ordinal() + numRanks - 1) % numRanks);
+    deck.sort(Comparator.comparingInt((card) ->
+        (card.getRank().ordinal() + numRanks - 1) % numRanks));
     System.out.println(deck);
   }
 
